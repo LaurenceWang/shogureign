10c357e67cbae847edef3d6b19b1bb517be44359
gluong <16323642+guy-luong@users.noreply.github.com>
2023-01-16 17:17:52 +0100

    ADD: Progress bar (not yet linked with cards)

--- PowerIndicators.js
+++ PowerIndicators.js
@@ -1,34 +1,34 @@
-import {includes} from 'lodash';
+import { includes } from 'lodash';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import PowerPerson from './PowerPerson';
+import { Images } from './images/index';
 
-const PowerIndicators = ({currentMood}) => {
+const PowerIndicators = ({ currentMood }) => {
   return (
     <>
       <View style={styles.wrapper}>
         <View style={styles.personsWrapper}>
           <PowerPerson
-            isHappy={includes(currentMood.happy, 'knight')}
-            isSad={includes(currentMood.sad, 'knight')}
-            image={'https://cdn-icons-png.flaticon.com/512/2701/2701847.png'}
+            isHappy={includes(currentMood.happy, 'happiness')}
+            isSad={includes(currentMood.sad, 'happiness')}
+            base={"happiness"}
           />
           <PowerPerson
-            isHappy={includes(currentMood.happy, 'joker')}
-            isSad={includes(currentMood.sad, 'joker')}
-            image={'https://cdn-icons-png.flaticon.com/512/5584/5584325.png'}
+            isHappy={includes(currentMood.happy, 'money')}
+            isSad={includes(currentMood.sad, 'money')}
+            base={"money"}
           />
           <PowerPerson
-            isHappy={includes(currentMood.happy, 'woman')}
-            isSad={includes(currentMood.sad, 'woman')}
-            image={'https://cdn-icons-png.flaticon.com/512/3275/3275703.png'}
+            isHappy={includes(currentMood.happy, 'popularity')}
+            isSad={includes(currentMood.sad, 'popularity')}
+            base={"popularity"}
           />
         </View>
       </View>
     </>
   );
 };
-
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
---
+++ images/index.js
@@ -0,0 +1,22 @@
+const Images = {
+  jaugeEffects: {
+    down: require('./jauge-effects/down-arrow.png'),
+    up: require('./jauge-effects/up-arrow.png'),
+  },
+  jauges: {
+    happiness: {
+      empty: require('./jauges/happiness_empty.png'),
+      full: require('./jauges/happiness.png')
+    },
+    money: {
+      empty: require('./jauges/money_empty.png'),
+      full: require('./jauges/money.png')
+    },
+    popularity: {
+      empty: require('./jauges/popularity_empty.png'),
+      full: require('./jauges/popularity.png')
+    }
+  }
+};
+
+export default Images;
